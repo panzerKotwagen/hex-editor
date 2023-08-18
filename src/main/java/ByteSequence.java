39c7add7e097ec95bccec2f940effc654c529125
kotb <kotyachy.andegraund@gmail.com>
2023-08-18 13:18:59 +0300

    Add function of searching for a byte mask in a sequence of bytes

--- src/main/java/ByteSequence.java
+++ src/main/java/ByteSequence.java
@@ -1,14 +1,11 @@
 import java.math.BigInteger;
+import java.util.Arrays;
 
 /**
  * A class for working with a sequence of bytes of a length 8.
  */
 public class ByteSequence {
     public static void main(String[] args) {
-        byte[] testBytes = new byte[] {32, 52, 53, 54, 32, 55, 56, 57};
-        ByteSequence byteSequence = new ByteSequence(testBytes);
-        BigInteger bi = byteSequence.representAsUnsigned64Bit();
-        System.out.println(bi);
     }
 
     /**
@@ -19,11 +16,13 @@ public class ByteSequence {
     /**
      * Default constructor.
      */
-    ByteSequence() {}
+    ByteSequence() {
+    }
 
     /**
      * Initializes byteSequence with bytes array values. If its length
      * is greater than 8, only the first 8 values will be recorded.
+     *
      * @param bytes
      */
     ByteSequence(byte... bytes) {
@@ -34,17 +33,46 @@ public class ByteSequence {
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ByteSequence that = (ByteSequence) o;
+        return Arrays.equals(byteSequence, that.byteSequence);
+    }
 
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(byteSequence);
+    }
+
+    /**
+     * Returns the byte at the specified index.
+     *
+     * @param pos the index of the byte
+     * @return the byte at the specified index
+     */
+    public byte getByte(int pos) {
+        return byteSequence[pos];
+    }
+
+    /**
+     * Converts a sequence of bytes to one long number.
+     *
+     * @param byteCount
+     * @return
+     */
     private long respresentAsLongNumber(int byteCount) {
         long res = 0;
         for (int i = 0; i < byteCount; i++) {
-            res += (long)(byteSequence[i] & 0xFF) << (8 * i);
+            res += (long) (byteSequence[i] & 0xFF) << (8 * i);
         }
         return res;
     }
 
     /**
      * Represents the first byte of the sequence as unsigned num.
+     *
      * @return
      */
     public long representAsUnsigned8Bit() {
@@ -53,6 +81,7 @@ public class ByteSequence {
 
     /**
      * Represents the first byte of the sequence as signed num.
+     *
      * @return
      */
     public byte representAsSigned8Bit() {
@@ -61,6 +90,7 @@ public class ByteSequence {
 
     /**
      * Represents the first 2 bytes of the sequence as unsigned num.
+     *
      * @return
      */
     public long representAsUnsigned16Bit() {
@@ -69,14 +99,16 @@ public class ByteSequence {
 
     /**
      * Represents the first 2 bytes of the sequence as signed num.
+     *
      * @return
      */
     public short representAsSigned16Bit() {
-        return (short)respresentAsLongNumber(2);
+        return (short) respresentAsLongNumber(2);
     }
 
     /**
      * Represents the first 4 bytes of the sequence as unsigned num.
+     *
      * @return
      */
     public long representAsUnsigned32Bit() {
@@ -85,35 +117,84 @@ public class ByteSequence {
 
     /**
      * Represents the first 4 bytes of the sequence as signed num.
+     *
      * @return
      */
     public int representAsSigned32Bit() {
-        return (int)respresentAsLongNumber(4);
+        return (int) respresentAsLongNumber(4);
     }
 
     /**
      * Represents the byte sequence as unsigned num.
+     *
      * @return
      */
     public BigInteger representAsUnsigned64Bit() {
         BigInteger UNSIGNED_LONG_MASK = BigInteger.ONE.shiftLeft(Long.SIZE).subtract(BigInteger.ONE);
         long unsignedLong = respresentAsLongNumber(8);
-        return  BigInteger.valueOf(unsignedLong).and(UNSIGNED_LONG_MASK);
+        return BigInteger.valueOf(unsignedLong).and(UNSIGNED_LONG_MASK);
     }
 
     /**
      * Represents the byte sequence as signed num.
+     *
      * @return
      */
     public long representAsSigned64Bit() {
         return respresentAsLongNumber(8);
     }
 
+    /**
+     * @return
+     */
     public float representAsFloat() {
-        return Float.intBitsToFloat((int)respresentAsLongNumber(4));
+        return Float.intBitsToFloat((int) respresentAsLongNumber(4));
     }
 
+    /**
+     * @return
+     */
     public double representAsDouble() {
         return Double.longBitsToDouble(respresentAsLongNumber(8));
+    }
+
+    /**
+     * Finds the position from which the mask equals the compared byte array.
+     *
+     * @param mask     the byte mask
+     * @param compared the byte sequence with which the mask is compared
+     * @return the index within this mask of the first occurrence of the
+     * specified byte array.
+     */
+    public static int find(byte[] mask, byte[] compared) {
+        if (mask.length > compared.length)
+            return -1;
+
+        if (compared.length > 8) return -1;
+
+        ByteSequence maskByteSequence = new ByteSequence(mask);
+        ByteSequence comparedByteSequence = new ByteSequence(compared);
+
+        long maskBits = maskByteSequence.representAsSigned64Bit();
+        long comparedPartBits = comparedByteSequence.representAsSigned64Bit();
+
+        long a = (long) Math.pow(2, mask.length * 8) - 1;
+        comparedPartBits = comparedPartBits & a; //cut to the size of the mask
+
+        for (int i = 0; i <= compared.length - mask.length; ++i) {
+            boolean res = (maskBits & comparedPartBits) == maskBits;
+
+            if (res == true)
+                return i;
+
+            // shift one byte to the right removing the lower digit
+            comparedPartBits = (comparedPartBits >> 8);
+
+            // insert a new byte on the left
+            byte addedByte = comparedByteSequence.getByte(mask.length + i);
+            comparedPartBits += (long) (addedByte & 0xFF) << 24;
+        }
+
+        return -1;
     }
 }
