68866ddd040db8db0923fc50453ec8ff21b575fd
kotb <kotyachy.andegraund@gmail.com>
2023-08-28 15:39:59 +0300

    Add menu and tool bars

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
@@ -19,9 +20,89 @@ public class MainWindow {
 
         menuBar = new JMenuBar();
 
+        makeFileMenu();
+        makeEditMenu();
+        makeHelpMenu();
+
+        makeToolBar();
+
+        frame.add(toolBar, BorderLayout.NORTH);
+
         frame.setJMenuBar(menuBar);
 
         frame.setVisible(true);
+    }
+
+    void makeFileMenu() {
+        JMenu menuFile = new JMenu("File");
+        menuFile.setMnemonic(KeyEvent.VK_F);
+
+        JMenuItem mItemOpen = new JMenuItem("Open", KeyEvent.VK_O);
+        JMenuItem mItemSave = new JMenuItem("Save", KeyEvent.VK_S);
+        JMenuItem mItemSaveAs = new JMenuItem("Save As", KeyEvent.VK_S);
+        JMenuItem mItemClose = new JMenuItem("Close", KeyEvent.VK_W);
+        JMenuItem mItemExit = new JMenuItem("Exit", KeyEvent.VK_Q);
+
+        mItemOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
+        mItemSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+        mItemSaveAs.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.SHIFT_MASK));
+        mItemClose.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_MASK));
+        mItemExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_MASK));
+
+        menuFile.add(mItemOpen);
+        menuFile.addSeparator();
+        menuFile.add(mItemSave);
+        menuFile.add(mItemSaveAs);
+        menuFile.addSeparator();
+        menuFile.add(mItemClose);
+        menuFile.add(mItemExit);
+
+        menuBar.add(menuFile);
+    }
+
+    void makeEditMenu() {
+        JMenu menuEdit = new JMenu("Edit");
+
+        JMenuItem mItemCopy = new JMenuItem("Copy");
+        JMenuItem mItemCut = new JMenuItem("Cut");
+        JMenuItem mItemPaste = new JMenuItem("Paste");
+        JMenuItem mItemFind = new JMenuItem("Find");
+
+        mItemCopy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_MASK));
+        mItemCut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_MASK));
+        mItemPaste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_MASK));
+        mItemFind.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_MASK));
+
+        menuEdit.add(mItemCopy);
+        menuEdit.add(mItemCut);
+        menuEdit.add(mItemPaste);
+        menuEdit.addSeparator();
+        menuEdit.add(mItemFind);
+
+        menuBar.add(menuEdit);
+    }
+
+    void makeHelpMenu() {
+        JMenu menuHelp = new JMenu("Help");
+        JMenuItem mItemAbout = new JMenuItem("About");
+        menuHelp.add(mItemAbout);
+
+        menuBar.add(menuHelp);
+    }
+
+    void makeToolBar() {
+        toolBar = new JToolBar("Tools");
+        toolBar.setFloatable(false);
+
+        JButton btnOpen = new JButton("O");
+        JButton btnClose = new JButton("C");
+        JButton btnSave = new JButton("S");
+        JButton btnSaveAs = new JButton("SA");
+
+        toolBar.add(btnOpen);
+        toolBar.add(btnClose);
+        toolBar.add(btnSave);
+        toolBar.add(btnSaveAs);
     }
 
     public static void main(String[] args) {
