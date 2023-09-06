99b9df9c932ccc6070c25f92a4349ec7baceb6a6
kotb <kotyachy.andegraund@gmail.com>
2023-09-06 20:41:02 +0300

    Add the dialog window for user input bytes
    The class describes dialog window in which a user can enter a block of bytes.

---
+++ src/main/java/gui/dialog/windows/InputDialogWindow.java
@@ -0,0 +1,68 @@
+package gui.dialog.windows;
+
+import gui.tables.InputTable;
+import gui.window.MainWindow;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * The class describes dialog window in which a user can enter a
+ * block of bytes.
+ */
+public class InputDialogWindow extends JFrame {
+    private byte[] data;
+    private JLabel label;
+    private JButton btnDo;
+    private JButton btnCancel;
+    private JPanel panelButtons;
+    private JPanel panelLabel;
+    private JScrollPane panelTable;
+
+    /**
+     * Constructs the dialog window.
+     * @param name window name
+     */
+    public InputDialogWindow(String name) {
+        super(name);
+
+        MainWindow.setUIFont(new javax.swing.plaf.FontUIResource("Arial", Font.PLAIN, 18));
+
+        this.setMinimumSize(new Dimension(420, 350));
+
+        this.setLocationRelativeTo(null);
+
+        this.setResizable(false);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        InputTable table = new InputTable();
+
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+
+        panelLabel = new JPanel(new FlowLayout());
+
+        panelLabel.add(label = new JLabel("Input the bytes"));
+
+        panelTable = new JScrollPane(table);
+
+        panelButtons = new JPanel(new FlowLayout());
+
+        panelButtons.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
+        panelTable.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+
+        btnDo = new JButton("Go");
+        btnCancel = new JButton("Cancel");
+
+        btnDo.addActionListener(e -> data = table.getData());
+
+        panelButtons.add(btnDo);
+        panelButtons.add(btnCancel);
+
+        this.add(panelLabel, BorderLayout.NORTH);
+        this.add(panelTable, BorderLayout.CENTER);
+        this.add(panelButtons, BorderLayout.SOUTH);
+
+        this.setVisible(true);
+    }
+}
